954b2ade476b9aaffacc90ac700d9136357306f1
v1lla <36949533+v1lla@users.noreply.github.com>
2021-06-14 00:27:18 +0200

    actualizacion de fichero desde otro branch

--- prueba.js
+++ prueba.js
@@ -1,7 +1,7 @@
 <script>
 
-  console.log("fichero de prueba");
+  console.log("fichero de prueba MODIFICADO EN OTRO BRANCH");
 
- console.log("fichero de prueba2222222");
+  console.log("fichero de prueba2222222 MODIFICADO EN OTRO BRANCH");
 
 <script>
